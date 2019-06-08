8c0a51f4e877d793f0e54a687bcc66ea1b35908f
fpfrimer <fpfirmer@gmail.com>
2019-06-08 11:44:21 -0300

    Corrigido código da aula 5

--- vhdl5_somador_completo/somador_completo.vhd
+++ vhdl5_somador_completo/somador_completo.vhd
@@ -1,4 +1,20 @@
 -- Curso de VHDL #5
 -- Exemplo de um somador completo
 -- Autor: Felipe W. D. Pfrimer
--- Data: 02/06/2019
+-- Data: 02/05/2019
+
+entity somador_completo is
+    port(
+        A, B    :   in  bit; -- entradas
+        TE      :   in  bit; -- Transporte de entrada
+        S       :   out bit; -- Soma
+        TS      :   out bit  -- Transposte de saída
+    );
+end entity;
+
+architecture main of somador_completo is
+begin
+    S <= A xor B xor TE;
+    TS <= (A and B) or (A and TE) or (B and TE);
+end architecture;
+-- Fim
