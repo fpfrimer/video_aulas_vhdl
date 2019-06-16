747651f1c7a3fb451653a4ec694b433a8b901722
fpfrimer <fpfirmer@gmail.com>
2019-06-16 13:18:42 -0300

    Adicionado aula 10

---
+++ vhdl10_when_else/mux_4_1/mux_4_1.vhd
@@ -0,0 +1,27 @@
+-- Curso de VHDL #10
+-- Exemplo de multiplexador com 4 entradas de 1 bit
+-- Autor: Felipe W. D. Pfrimer
+-- Data: 16/06/2019
+
+entity mux_4_1 is
+  port (
+    D0, D1, D2, D3  :   in  bit;
+    S0, S1          :   in  bit;
+    Y               :   out bit
+  ) ;
+end mux_4_1;
+
+architecture main of mux_4_1 is
+
+    signal sel  :   bit_vector(1 downto 0):= "00";
+
+begin
+
+    sel <= S1 & S0;
+
+    Y <=    D0 when sel = "00" else
+            D1 when sel = "01" else
+            D2 when sel = "10" else
+            D3;
+
+end main ; -- main
