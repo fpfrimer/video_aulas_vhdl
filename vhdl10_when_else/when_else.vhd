adce8e28e32c9f2579f38236df8354d39202512d
fpfrimer <fpfirmer@gmail.com>
2019-06-16 13:29:09 -0300

    Outra atualização da aula 10

--- vhdl10_when_else/when_else.vhd
+++ vhdl10_when_else/when_else.vhd
@@ -1,8 +1,8 @@
 architecture behavioral of ent is
 begin
 
-    signal_name 	<= 	expression_1 when condition_1 else
-                        expression_2 when condition_2 else
-                        expression_3 ;
-                        
+    signal_name <= 	expression_1 when condition_1 else
+                    expression_2 when condition_2 else
+                    expression_3 ;
+
 end behavioral ; -- arch
